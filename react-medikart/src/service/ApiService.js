f6f0719b63e91ea33f175bb800ec4367285790c1
Srishti Kumari <srishve13@gmail.com>
2026-05-21 18:29:11 +0530

    Fix HTTP backend URL

--- react-medikart/src/service/ApiService.js
+++ react-medikart/src/service/ApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default class ApiService {
-  static BASE_URL = "http://medikart-backend-im3c.onrender.com";
+  static BASE_URL = "https://medikart-backend-im3c.onrender.com";
 
   static getHeader() {
     const token = localStorage.getItem("token");
