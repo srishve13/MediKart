eefff34b565e99f9f5631bd6367dd88ea4c8f283
Srishti Kumari <srishve13@gmail.com>
2026-05-26 15:34:45 +0530

    Fix repository search method

--- MediKart/src/main/java/com/medikart/MediKart/repository/ProductRepo.java
+++ MediKart/src/main/java/com/medikart/MediKart/repository/ProductRepo.java
@@ -2,11 +2,11 @@ package com.medikart.MediKart.repository;
 
 import java.util.List;
 
-import com.medikart.MediKart.entity.Product;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.medikart.MediKart.entity.Product;
 public interface ProductRepo extends JpaRepository<Product, Long>{
     List<Product> findByCategoryId(Long categoryId);
-    List<Product> findByNameContainingOrDescriptionContaining(String name, String description);
+    List<Product> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String name, String description);
 
 }
